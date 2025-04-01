62bd730751d78319ea6d6e7acf2e83b60c9407b9
phareedahadamu <phareedahadamu@gmail.com>
2025-04-01 15:37:41 +0100

    fix: redirect modal overflow

--- components/RedirectModal.tsx
+++ components/RedirectModal.tsx
@@ -6,7 +6,7 @@ export default function RedirectModal({
   close: (value: boolean) => void;
 }) {
   return (
-    <div className="backdrop-blur-[12px] bg-[#00000033] h-[100dvh] absolute top-[0px] z-50 w-full mt-[-72.5px] flex items-center justify-center">
+    <div className="no_scroll backdrop-blur-[12px] bg-[#00000033] h-[100dvh] absolute top-[0px] z-50 w-full mt-[-72.5px] flex items-center justify-center">
       <div className="flex px-[24px] py-[12px] rounded-[8px] bg-gray-50 border-[1px] border-gray-700 text-gray-900 gap-[16px] text-[18px]">
         <p>
           You have to be signed in to use this feature.{" "}
